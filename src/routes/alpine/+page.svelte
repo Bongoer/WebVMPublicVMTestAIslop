c5b7e387fc9a02a154af879e2252ab4f646db619
Bongoar <kohlanyj@proton.me>
2026-07-24 08:06:57 +0000

    Add XFCE browser VM

---
+++ src/routes/alpine/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import WebVM from '$lib/WebVM.svelte';
+  import * as configObj from '/config_alpine';
+</script>
+
+<svelte:head>
+  <title>XFCE Linux WebVM</title>
+  <meta
+    name="description"
+    content="An independent Alpine Linux XFCE virtual machine running in your browser."
+  />
+</svelte:head>
+
+<WebVM configObj={configObj} cacheId="blocks_xfce">
+  <p>
+    This XFCE Linux machine runs locally in your browser. Each visitor gets a
+    separate virtual machine.
+  </p>
+</WebVM>
